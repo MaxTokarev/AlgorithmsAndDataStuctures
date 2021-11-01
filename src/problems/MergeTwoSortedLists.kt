086424f472a21b8cb7ed2a53c105a2ba2b9a17a4
maksimilian <maksim_tokarev_111@mail.ru>
2021-11-01 08:46:40 +0300

    Add problem - merge two sorted lists

---
+++ src/problems/MergeTwoSortedLists.kt
@@ -0,0 +1,56 @@
+package problems
+
+/**
+ * Problem - https://leetcode.com/problems/merge-two-sorted-lists/
+ *
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+interface MergeTwoSortedListsSolution {
+    fun mergeTwoLists(l1: ListNode?, l2: ListNode?): ListNode?
+
+    data class ListNode(var `val`: Int) {
+        var next: ListNode? = null
+    }
+
+    class Own : MergeTwoSortedListsSolution {
+        override fun mergeTwoLists(l1: ListNode?, l2: ListNode?): ListNode? {
+            return when {
+                l1 == null && l2 == null -> null
+                l1 == null && l2 != null -> l2
+                l1 != null && l2 == null -> l1
+                l1!!.`val` < l2!!.`val` -> ListNode(l1.`val`).apply {
+                    next = mergeTwoLists(l1.next, l2)
+                }
+                else -> ListNode(l2.`val`).apply {
+                    next = mergeTwoLists(l1, l2.next)
+                }
+            }
+        }
+    }
+
+    companion object {
+        /** For tests */
+        fun generateNodes(vararg values: Int): ListNode? {
+            fun IntArray.remove(index: Int): IntArray {
+                return if (index < 0 || index >= size) this
+                else indices.filter { it != index }
+                    .map { get(it) }
+                    .toIntArray()
+            }
+            return when {
+                values.isEmpty() -> null
+                else -> {
+                    ListNode(values.first()).apply {
+                        next = generateNodes(*values.remove(0))
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ test/problems/MergeTwoSortedListsSolutionTest.kt
@@ -0,0 +1,35 @@
+package problems
+
+import org.junit.Test
+import kotlin.test.assertTrue
+
+internal class MergeTwoSortedListsSolutionTest {
+    private val merger = MergeTwoSortedListsSolution.Own()
+
+    @Test
+    fun `test two equal nodes`() {
+        val node1 = generateNodesSorted(1, 2, 3)
+        val node2 = generateNodesSorted(1, 2, 3)
+        val expected = merger.mergeTwoLists(node1, node2)
+        val actual = generateNodesSorted(1, 1, 2, 2, 3, 3)
+        assertTrue { expected == actual }
+    }
+
+    @Test
+    fun `test empty nodes`() {
+        val emptyNode = generateNodesSorted()
+        val expected = merger.mergeTwoLists(emptyNode, emptyNode)
+        assertTrue { expected == emptyNode }
+    }
+
+    @Test
+    fun `test one empty node`() {
+        val node1 = generateNodesSorted(1,14)
+        val node2 = generateNodesSorted()
+        val actual = merger.mergeTwoLists(node1, node2)
+        assertTrue { actual == node1 }
+    }
+
+    private fun generateNodesSorted(vararg values: Int) =
+        MergeTwoSortedListsSolution.generateNodes(*values.sorted().toIntArray())
+}
